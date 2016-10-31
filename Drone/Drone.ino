9fe10396c35827d079937a2c40e67574ec690ff4
Weili An <China_Aisa@live.com>
2016-10-31 19:29:04 +0800

    First commit, Hello World!

---
+++ Drone/Drone.ino
@@ -0,0 +1,28 @@
+#include <helper_3dmath.h>
+#include <MPU6050.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050_9Axis_MotionApps41.h>
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+/* =========================================================================
+   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the Arduino's
+   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
+   digital I/O pin 2.
+ * ========================================================================= */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
