f1683a743b6c15366fda89450f943d1fe55c2668
Weili An <China_Aisa@live.com>
2016-11-16 08:18:46 +0800

    Start working!

--- Drone/Drones.ino/Drones.ino.ino
+++ Drone/Drones.ino/Drones.ino.ino
@@ -1,22 +1,32 @@
-#include <helper_3dmath.h>
-#include <MPU6050.h>
+//Core library
 #include <MPU6050_6Axis_MotionApps20.h>
-#include <MPU6050_9Axis_MotionApps41.h>
+#include <I2Cdev.h>
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
+//Specify the I2C address of MPU6050, default I2C adress is 0x68
 MPU6050 mpu;
-//MPU6050 mpu(0x69); // <-- use for AD0 high
 /* =========================================================================
    NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
    depends on the MPU-6050's INT pin being connected to the Arduino's
    external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
    digital I/O pin 2.
  * ========================================================================= */
+//uncomment the following define in order to get the information you want
+//#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_YAWPITCHROLL
+//Uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration components with gravity removed.
+#define OUTPUT_READABLE_REALACCEL
+
+
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
