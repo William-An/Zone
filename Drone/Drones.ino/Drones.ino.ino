3ce6032ae3eebaae70ae97ffbeadff384f8a375c
Weili An <China_Aisa@live.com>
2016-12-09 21:49:19 +0800

    Adding softSerial reading

--- Drone/Drones.ino/Drones.ino.ino
+++ Drone/Drones.ino/Drones.ino.ino
@@ -1,25 +1,61 @@
 //Core library
 #include <MPU6050_6Axis_MotionApps20.h>
 #include <I2Cdev.h>
+#include <SoftwareSerial.h>
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
+// SoftwareSerial.h is used to read data from GPS without interrupt with the Xbee
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
 //Specify the I2C address of MPU6050, default I2C adress is 0x68
 MPU6050 mpu;
-/* =========================================================================
-   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-   depends on the MPU-6050's INT pin being connected to the Arduino's
-   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
-   digital I/O pin 2.
- * ========================================================================= */
 //uncomment the following define in order to get the information you want
 //#define OUTPUT_READABLE_QUATERNION
 //#define OUTPUT_READABLE_EULER
 #define OUTPUT_READABLE_YAWPITCHROLL
 //Uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration components with gravity removed.
 #define OUTPUT_READABLE_REALACCEL
+#define INTERRUPT_PIN 2
+#define LED_PIN 12
+#define MOTOR_1_PIN 3
+#define MOTOR_2_PIN 5
+#define MOTOR_3_PIN 6
+#define MOTOR_4_PIN 9
+#define SOFT_SERIAL_TX 11
+#define SOFT_SERIAL_RX 12 
+int throttle_stick = 0;
+int yaw_stick = 0;
+int pitch_stick = 0;
+int roll_stick = 0;
+int motor_1_output;
+int motor_2_output;
+int motor_3_output;
+int motor_4_output;
+bool blinkState = false;
+bool dmpReady = false;
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+char gpsbuffer[64];
+// Initialize buffer for gps data
+char xbeebuffer[10];
+// Initialize buffer for controller
+SoftwareSerial gpsSerial (SOFT_SERIAL_RX,SOFT_SERIAL_TX);
 
 
 
@@ -28,11 +64,32 @@ MPU6050 mpu;
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+	pinMode(MOTOR_1_PIN, OUTPUT);
+	pinMode(MOTOR_2_PIN, OUTPUT);
+	pinMode(MOTOR_3_PIN, OUTPUT);
+	pinMode(MOTOR_4_PIN, OUTPUT);
+	// Define all motor pins as output
+	pinMode(SOFT_SERIAL_RX, INPUT);
+	pinMode(SOFT_SERIAL_TX, OUTPUT);
+	// Define softSerial pins' mode 
+	Serial.begin(9600);
+	gpsSerial.begin(9600);
+	// Begin both serials
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 	if (gpsSerial.available() > 0){//need to change to gpsSerial
+  		for(int i=0;i<64;i++){
+  			gpsbuffer[i] = gpsSerial.read(); // Read data from gps and store them in buffer
+ 		    gpsSerial.flush();
+ 		}
+	}
+  for(int i;i<64;i++){
+  	Serial.print(gpsbuffer[i]);
+    gpsbuffer[i]=0;
+  }
+//	For Testing only
+
 
 }
