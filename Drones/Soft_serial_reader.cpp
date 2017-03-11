12f8a43cba2ba1c053095ee3ebe5675cf16441d7
Weili An <China_Aisa@live.com>
2017-03-12 01:46:22 +0800

    Finish MPU & BMP code

--- Drones/Soft_serial_reader.cpp
+++ Drones/Soft_serial_reader.cpp
@@ -2,6 +2,21 @@
 #include "Soft_serial_reader.h"
 #include "String_process.h"
 #include <SoftwareSerial.h>
+/*
+GPRMC Structure
+Where:
+    RMC          Recommended Minimum sentence C
+    123519       Fix taken at 12:35:19 UTC
+    A            Status A=active or V=Void.
+    4807.038,N   Latitude 48 deg 07.038' N
+    01131.000,E  Longitude 11 deg 31.000' E
+    022.4        Speed over the ground in knots
+    084.4        Track angle in degrees True
+    230394       Date - 23rd of March 1994
+    003.1,W      Magnetic Variation
+    *6A          The checksum data, always begins with *
+From http://www.gpsinformation.org/dale/nmea.htm 
+*/
 char* dynamic_array_serial(SoftwareSerial src, int max_length, char start_char, char terminator) {
   src.listen();//Open serial
   while (src.available() <= 10); //Waiting for bytes to fill buffer, set 10 in order to allow more bytes loaded in buffer
